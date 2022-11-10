80e36f7591667d58916dc91ff1d71e11acc64233
ViktoryiaKolbantseva <108792405+ViktoryiaKolbantseva@users.noreply.github.com>
2022-11-10 12:10:45 +0300

    Update controls.py

--- controls.py
+++ controls.py
@@ -102,3 +102,14 @@ def input_complex():
                     logger_calc.result_logger(result=message)
                     break
     return nums_to_work
+
+
+
+from datetime import datetime
+
+
+def result_logger(result):
+    current_date = datetime.now()
+    current_date_string = current_date.strftime(' %m/%d/%y %H:%M')
+    with open('log.csv', 'a', encoding='cp1251') as file:
+        file.write(f'{current_date_string}  результат: {result}\n')
